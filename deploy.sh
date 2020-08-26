9fe55d6c44627a7de5e6b3831e6e24b0346ab50c
Femi Oladele <profemzy@gmail.com>
2020-08-26 20:59:45 +0100

    Update Image Repo

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t profemzy/multi-client:latest -t profemzy/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t profemzy/multi-server:latest -t profemzy/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t profemzy/multi-worker:latest -t profemzy/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push profemzy/multi-client:latest
+docker push profemzy/multi-server:latest
+docker push profemzy/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push profemzy/multi-client:$SHA
+docker push profemzy/multi-server:$SHA
+docker push profemzy/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=profemzy/multi-server:$SHA
+kubectl set image deployments/client-deployment client=profemzy/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=profemzy/multi-worker:$SHA
